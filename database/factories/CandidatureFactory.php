e44d720e83eece5e709553da1ac4dbff1e0c48a6
BEN-ESSAHRAOUI-Yassine <ybenessahraoui@gmail.com>
2026-05-20 19:57:01 +0100

    add factory

--- database/factories/CandidatureFactory.php
+++ database/factories/CandidatureFactory.php
@@ -18,7 +18,21 @@ class CandidatureFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'entreprise' => fake()->company(),
+            'poste' => fake()->jobTitle(),
+            'offre_url' => fake()->url(),
+            'statut' => fake()->randomElement([
+                'to_review',
+                'interview_scheduled',
+                'offer_received'
+            ]),
+            'priorite' => fake()->randomElement([
+                'high',
+                'medium',
+                'low'
+            ]),
+            'notes' => fake()->paragraph(),
+            'date_candidature' => now()
         ];
     }
 }
--- database/factories/EntretienFactory.php
+++ database/factories/EntretienFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Entretien;
+use App\Models\Candidature;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,7 +19,50 @@ class EntretienFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+
+            'candidature_id' => Candidature::factory(),
+
+            'type' => fake()->randomElement([
+                'telephone',
+                'technique',
+                'rh',
+                'final'
+            ]),
+
+            'date_entretien' => fake()->dateTimeBetween(
+                'now',
+                '+1 month'
+            ),
+
+            'notes_preparation' => fake()->paragraph(),
+
+            'resultat' => fake()->randomElement([
+                'pending',
+                'positive',
+                'negative'
+            ])
         ];
+
+    }
+
+    public function pending(): static
+    {
+        return $this->state(fn () => [
+            'resultat' => 'pending'
+        ]);
+    }
+
+    public function positive(): static
+    {
+        return $this->state(fn () => [
+            'resultat' => 'positive'
+        ]);
+    }
+
+    public function negative(): static
+    {
+        return $this->state(fn () => [
+            'resultat' => 'negative'
+        ]);
     }
 }
